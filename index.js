3c59b439eddddf88d42f2b43607e7c824a66a38f
elementsweb <elementsweb@users.noreply.github.com>
2017-11-03 16:54:16 +0000

    Made module executable from command line

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
+#!/usr/bin/env node
 const yargs = require('yargs');
 
 yargs
   .commandDir('cmds')
   .argv;
-
-// use current directory by default
-// directory as positional argument
-// flag to go into all directories in the positional argument
